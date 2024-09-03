382d95a3c4266741d99109ba1e3a83c2661c2a2c
SalvatoreCorsaro <salvatore.corsaro@>
2024-09-03 18:41:34 +0200

    :tada: ArrayList

---
+++ src/HelloArrayList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloArrayList {
+
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> nameList = new ArrayList<>();
+
+        nameList.add("Paco");
+        nameList.add("Bob");
+        nameList.add("Jack");
+
+        System.out.println(nameList);
+        nameList.add("Mary");
+        System.out.println(nameList);
+
+
+        System.out.println( "Does it contain Bob?: " + nameList.contains("Bob"));
+        nameList.remove("Bob");
+        System.out.println( "Does it contain Bob?: " + nameList.contains("Bob"));
+
+        nameList.set(0, "Charlie");
+
+        for (int i = 0; i < nameList.size(); i++) {
+            System.out.println(i + ") " + nameList.get(i));
+        }
+
+        ArrayList<Integer> numList = new ArrayList<>(List.of(1, 2, 3, 4));
+        System.out.println(numList);
+    }
+}
